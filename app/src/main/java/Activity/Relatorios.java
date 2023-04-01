1e93249237d1ceac5363752301c2b661ae97395c
Jackiany <jackiany14@gmail.com>
2023-04-01 15:48:01 -0300

    Initial commit

---
+++ app/src/main/java/Activity/Relatorios.java
@@ -0,0 +1,16 @@
+package Activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.fisga.R;
+
+public class Relatorios extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relatorios);
+    }
+}
